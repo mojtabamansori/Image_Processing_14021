5fad67c48d489f627af2656d780bc239557239d5
mojtabamansori <13mrlien13@gmail.com>
2023-10-10 11:33:51 +0330

    start tamrin 2

--- tamrin_1_bicubic/function_bicubic_rotation.py
+++ tamrin_1_bicubic/function_bicubic_rotation.py
@@ -12,7 +12,6 @@ import math
 def rotate_bicubic(image, angle):
     print('rotate bi cubic is run ')
     height, width, _ = image.shape
-    rotated_image = np.zeros((height, width, 3), dtype=np.uint8)
 
     # Calculate the rotation angle in radians.
     theta = math.radians(angle)
@@ -20,6 +19,21 @@ def rotate_bicubic(image, angle):
     # Calculate the coordinates of the center of the image.
     center_x = width / 2
     center_y = height / 2
+
+    # Calculate the diagonal of the image to determine the size of the padding.
+    diagonal = math.sqrt(width**2 + height**2)
+    padding_x = int((diagonal - width) / 2)
+    padding_y = int((diagonal - height) / 2)
+
+    # Create a padded image with zeros.
+    padded_image = np.zeros((height + 2 * padding_y, width + 2 * padding_x, 3), dtype=np.uint8)
+    padded_image[padding_y:padding_y + height, padding_x:padding_x + width] = image
+
+    # Calculate the new coordinates of the center after padding.
+    center_x += padding_x
+    center_y += padding_y
+
+    rotated_image = np.zeros((height, width, 3), dtype=np.uint8)
 
     for i in range(height):
         print(f'\rrun rotation: [{i}/{height}]', end='')
@@ -30,8 +44,8 @@ def rotate_bicubic(image, angle):
             new_x = x * math.cos(theta) - y * math.sin(theta) + center_x
             new_y = x * math.sin(theta) + y * math.cos(theta) + center_y
 
-            # Check if the new coordinates are within the bounds of the image.
-            if 0 <= new_x < width and 0 <= new_y < height:
+            # Check if the new coordinates are within the bounds of the padded image.
+            if 0 <= new_x < width + 2 * padding_x and 0 <= new_y < height + 2 * padding_y:
                 # Perform bicubic interpolation using the provided bicubic_kernel function.
                 x1, y1 = int(new_x), int(new_y)
                 dx = new_x - x1
@@ -42,10 +56,10 @@ def rotate_bicubic(image, angle):
                     channel_value = 0
                     for m in range(-1, 3):
                         for n in range(-1, 3):
-                            x_index = min(max(x1 + n, 0), width - 1)
-                            y_index = min(max(y1 + m, 0), height - 1)
+                            x_index = min(max(x1 + n, 0), width + 2 * padding_x - 1)
+                            y_index = min(max(y1 + m, 0), height + 2 * padding_y - 1)
                             coefficient = bicubic_kernel(dx - n) * bicubic_kernel(dy - m)
-                            channel_value += coefficient * image[y_index, x_index, c]
+                            channel_value += coefficient * padded_image[y_index, x_index, c]
 
                     interpolated_pixel[c] = np.clip(int(channel_value), 0, 255)
 
--- tamrin_1_bicubic/main.py
+++ tamrin_1_bicubic/main.py
@@ -21,32 +21,32 @@ new_x = int(int(original_image.shape[0]) * upscale_factor)
 new_y = int(int(original_image.shape[1]) * upscale_factor)
 
 # up_sample section
-up_scaled_data_cubic = bicubic_interpolation(original_image, (new_x,new_y))
-up_scaled_data_nn = upscale_nearest_neighbor(original_image, upscale_factor)
-up_scaled_data_linear = bilinear_interpolation(original_image, upscale_factor)
+# up_scaled_data_cubic = bicubic_interpolation(original_image, (new_x,new_y))
+# up_scaled_data_nn = upscale_nearest_neighbor(original_image, upscale_factor)
+# up_scaled_data_linear = bilinear_interpolation(original_image, upscale_factor)
 rotate_image = rotate_bicubic(original_image, angle)
 
-# save section
-cv2.imwrite(f"original_image.jpg", original_image)
-cv2.imwrite(f"up_scaled_image_bicubic_{new_x}.jpg", up_scaled_data_cubic)
-cv2.imwrite(f"up_scaled_image_nn_{new_x}.jpg", up_scaled_data_nn)
-cv2.imwrite(f"up_scaled_image_linear_{new_x}.jpg", up_scaled_data_linear)
+# # save section
+# cv2.imwrite(f"original_image.jpg", original_image)
+# cv2.imwrite(f"up_scaled_image_bicubic_{new_x}.jpg", up_scaled_data_cubic)
+# cv2.imwrite(f"up_scaled_image_nn_{new_x}.jpg", up_scaled_data_nn)
+# cv2.imwrite(f"up_scaled_image_linear_{new_x}.jpg", up_scaled_data_linear)
 cv2.imwrite(f"rotate_image_{angle}.jpg", rotate_image)
 
-
-# plot section
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 10), sharex=True, sharey=True)
-ax[0, 0].imshow(original_image, cmap='gray', vmin=0, vmax=255)
-ax[0, 0].set_title('Original Image')
-ax[0, 1].imshow(up_scaled_data_linear, cmap='gray', vmin=0, vmax=255)
-ax[0, 1].set_title(f'Up_scaled Image, bi linear')
-ax[1, 0].imshow(up_scaled_data_nn, cmap='gray', vmin=0, vmax=255)
-ax[1, 0].set_title(f'Up_scaled Image, nn')
-ax[1, 1].imshow(up_scaled_data_cubic, cmap='gray', vmin=0, vmax=255)
-ax[1, 1].set_title(f'Up_scaled Image, Bi-cubic')
-plt.show()
-
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10), sharex=True, sharey=True)
-ax.imshow(rotate_image, cmap='gray', vmin=0, vmax=255)
-ax.set_title(f'Rotated Image')  # عنوان شکل دوم
-plt.show()
+#
+# # plot section
+# fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 10), sharex=True, sharey=True)
+# ax[0, 0].imshow(original_image, cmap='gray', vmin=0, vmax=255)
+# ax[0, 0].set_title('Original Image')
+# ax[0, 1].imshow(up_scaled_data_linear, cmap='gray', vmin=0, vmax=255)
+# ax[0, 1].set_title(f'Up_scaled Image, bi linear')
+# ax[1, 0].imshow(up_scaled_data_nn, cmap='gray', vmin=0, vmax=255)
+# ax[1, 0].set_title(f'Up_scaled Image, nn')
+# ax[1, 1].imshow(up_scaled_data_cubic, cmap='gray', vmin=0, vmax=255)
+# ax[1, 1].set_title(f'Up_scaled Image, Bi-cubic')
+# plt.show()
+#
+# fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10), sharex=True, sharey=True)
+# ax.imshow(rotate_image, cmap='gray', vmin=0, vmax=255)
+# ax.set_title(f'Rotated Image')  # عنوان شکل دوم
+# plt.show()
